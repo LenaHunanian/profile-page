33d460a45f34d022573e09d3586641969725445e
LenaHunanian <lenasss20053112@gmail.com>
2025-06-26 21:12:58 +0400

    Update ViewController.swift added missing view background color

--- profile-page/ViewController.swift
+++ profile-page/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         configureStackView()
         setupUI()
         configureStacks()
